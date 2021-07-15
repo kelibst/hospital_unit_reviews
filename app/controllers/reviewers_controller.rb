acdd2be5d99550e8aaa1cd1167f91ffe9a909f94
Keli Booster <kbooster17@gmail.com>
2021-07-15 03:54:57 +0000

    find reviewer by username

--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -45,7 +45,7 @@ class ReviewersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_reviewer
-      @reviewer = Reviewer.find_by_phone!(params[:id])
+      @reviewer = Reviewer.find_by_username!(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
